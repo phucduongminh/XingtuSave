9e90d0a7583b224a32713daf6eda94cade245275
phucduongminh <phucbopbnation@gmail.com>
2024-01-01 23:21:47 +0700

    ko lam dc gi

--- src/controllers/connectDB.ts
+++ src/controllers/connectDB.ts
@@ -2,5 +2,5 @@ import { enablePromise, openDatabase } from 'react-native-sqlite-storage';
 
 enablePromise(true);
 export const getDBConnection = async () => {
-    return openDatabase({name : "players1.db", createFromLocation : "~players1.db"});
+    return openDatabase({name : "players1.db", location:"default"});
 };
